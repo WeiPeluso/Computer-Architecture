1028d6609ddcf379108d9ba88c189bd989d2cc43
Wei Peluso <weipeluso@MacBook-Pro.local>
2020-09-16 14:08:52 -0400

    day 2 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,6 +1,7 @@
 """CPU functionality."""
 
 import sys
+
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
@@ -20,12 +21,6 @@ NOP = 0b00000000
 LD = 0b10000011
 ST = 0b10000100
 CMP = 0b10100111
-INC = 0b01100101
-DEC = 0b01100110
-PUSH = 0b01000101
-POP = 0b01000110
-CALL = 0b01010000
-RET = 0b00010001
 
 
 class CPU:
@@ -44,33 +39,42 @@ class CPU:
     def ram_write(self, position, command):
         self.ram[position] = command
 
-    def load(self):
+    def load(self, file):
         """Load a program into memory."""
 
         address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010,  # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111,  # PRN R0
+        #     0b00000000,
+        #     0b00000001,  # HLT
+        # ]
+        try:
+            with open(file) as f:
+                for line in f:
+                    t = line.split('#')
+                    n = t[0].strip()
+                    if n == "":
+                        continue
+                    command = int(n, 2)
+                    self.ram[address] = command
+                    address += 1
+        except FileNotFoundError:
+            print(f"File not found: {file}")
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+            # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -99,14 +103,18 @@ class CPU:
         self.running = True
         while self.running:
             op = self.ram_read(self.pc)
+            op_len = ((op & 0b11000000) >> 6) + 1
             if op == HLT:
                 self.running = False
             elif op == LDI:
                 reg_num = self.ram[self.pc + 1]
                 value = self.ram[self.pc + 2]
                 self.registers[reg_num] = value
-                self.pc += 3
             elif op == PRN:
                 reg_num = self.ram[self.pc + 1]
                 print(self.registers[reg_num])
-                self.pc += 2
+            elif op == MUL:
+                operand_a = self.ram[self.pc + 1]
+                operand_b = self.ram[self.pc+2]
+                self.registers[operand_a] *= self.registers[operand_b]
+            self.pc += op_len
